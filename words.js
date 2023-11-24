97e38ec2c470346bf6b247a1e7673cd949e01412
zbien12 <151922105+zbien12@users.noreply.github.com>
2023-11-24 17:17:52 +0700

    Add files via upload

---
+++ words.js
@@ -0,0 +1,162 @@
+const wordList = [
+    {
+        word: "rakha",
+        hint: "sumber jawaban"
+    },
+    {
+        word: "adwa",
+        hint: "mulut bagai kebun binatang"
+    },
+    {
+        word: "masamos",
+        hint: "aslab paling ganteng"
+    },
+    {
+        word: "bayu",
+        hint: "suhu coding"
+    },
+    {
+        word: "salma",
+        hint: "si paling ibuk"
+    },
+    {
+        word: "prima",
+        hint: "barudak well"
+    },
+    {
+        word: "siftiyadin",
+        hint: "sss haahhh"
+    },
+    {
+        word: "shabrina",
+        hint: "kos kok mie ayam (ytta)"
+    },
+    {
+        word: "pakiyan",
+        hint: "pro player pingpong & badminton"
+    },
+    {
+        word: "alim",
+        hint: "paling tepat waktu"
+    },
+    {
+        word: "vivo",
+        hint: "saingan oppo"
+    },
+    {
+        word: "dina",
+        hint: "hari"
+    },
+    {
+        word: "anggi",
+        hint: "temennya hari"
+    },
+    {
+        word: "dwi",
+        hint: "yang kedua"
+    },
+    {
+        word: "diani",
+        hint: "bukan aku dan bukan kamu"
+    },
+    {
+        word: "arya",
+        hint: "kos kok sate ayam"
+    },
+    {
+        word: "dimasss",
+        hint: "partner hidup satrio"
+    },
+    {
+        word: "satrio",
+        hint: "berpasir"
+    },
+    {
+        word: "qeis",
+        hint: "calon suami member jkt48"
+    },
+    {
+        word: "riris",
+        hint: "crush sss haahhh"
+    },
+    {
+        word: "adam",
+        hint: "jangan salah pencet kelas bro"
+    },
+    {
+        word: "raysad",
+        hint: "baju tembus"
+    },
+    {
+        word: "zaki",
+        hint: "operator kita"
+    },
+    {
+        word: "jeremia",
+        hint: "pro player ML"
+    },
+    {
+        word: "adika",
+        hint: "orang bekasi"
+    },
+    {
+        word: "khalim",
+        hint: "jedag jedug for life"
+    },
+    {
+        word: "feroland",
+        hint: "fashionable"
+    },
+    {
+        word: "andra",
+        hint: "backbone"
+    },
+    {
+        word: "sulthan",
+        hint: "bang masuk bang"
+    },
+    {
+        word: "raena",
+        hint: "bu kha"
+    },
+    {
+        word: "nawar",
+        hint: "ibu-ibu di pasar"
+    },
+    {
+        word: "ezra",
+        hint: "wibu akut"
+    },
+    {
+        word: "aurel",
+        hint: "naks pondok"
+    },
+    {
+        word: "zizka",
+        hint: "sultan"
+    },
+    {
+        word: "stefani",
+        hint: "+30 menit"
+    },
+    {
+        word: "haila",
+        hint: "baskoro"
+    },
+    {
+        word: "sabina",
+        hint: "si paling sibuk"
+    },
+    {
+        word: "riska",
+        hint: "apin (ytta)"
+    },
+    {
+        word: "billa",
+        hint: "terbuka"
+    },
+    {
+        word: "afra",
+        hint: "danusan for lyfe"
+    },
+]
